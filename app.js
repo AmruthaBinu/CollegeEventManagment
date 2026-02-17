61c587682c2248d5f7bf6d32bb8646b9ff30802a
AmruthaBinu <amruthabinu133@gmail.com>
2026-02-17 17:35:28 +0530

    signin completed

--- app.js
+++ app.js
@@ -12,7 +12,43 @@ app.use(Cors())
 
 Mongoose.connect("mongodb+srv://amruthabinu:amruthabinu2002@cluster0.bwn2sfy.mongodb.net/collegeEventDb?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get("/signup",(req,res)=>{
+
+//SIGNIN
+app.post("/signin",(req,res)=>{
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+
+                const passwordValidator=Bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordValidator) {
+                    Jwt.sign({email:req.body.email},"collegeEventApp",{expiresIn:"1d"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status":"error","errorMessage":error})
+                            } else {
+                                res.json({"status":"success","token":token,"userId":items[0]._id})
+                            }
+                        }
+                    )
+                    
+                } else {
+                    res.json({"status":"Incorrect password"})
+                    
+                }
+                
+            } else {
+                res.json({"status":"Invalid email id"})
+            }
+
+        }
+    ).catch()
+})
+
+
+
+//SIGNUP
+app.post("/signup",(req,res)=>{
     
     let input=req.body
     let hashedPassword = Bcrypt.hashSync(req.body.password,10)
